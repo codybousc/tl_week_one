b27be1b247dec797c0b73e7ad104a30abbe4a178
Cody Bouscaren <codybousc@gmail.com>
2015-03-02 19:35:57 -0800

    update tic tac toe

--- tic_tac/tic_tac.rb
+++ tic_tac/tic_tac.rb
@@ -1,14 +1,8 @@
-
-
-
 def initialize_board
   b = {}
   (1..9).each {|position| b[position] = ' '}
   b 
 end
-
-
-
 
 def draw_board(b)
   system 'clear'
@@ -19,29 +13,27 @@ def draw_board(b)
   puts " #{b[7]} | #{b[8]}   | #{b[9]}   |"
 end
 
-
-def player_pick_square(b)
+def player_pick_square(board)
   begin
     puts "Pick a position, my friend! (1 through 9)"
     position = gets.chomp.to_i
-    valid_choice = empty_positions(b).include? position
+    valid_choice = empty_positions(board).include? position
     if !valid_choice
       puts "Please chose an empty square!"
     end 
   end until valid_choice
 
-b[position] = 'X'
+board[position] = 'X'
 end
 
-def empty_positions(b)
-  b.select {|key, value| value == ' '}.keys
+def empty_positions(board)
+  board.select {|_, value| value == ' '}.keys
 end
 
-def computer_picks_square(b)
-  position = empty_positions(b).sample
-  b[position] = 'O'
+def computer_picks_square(board)
+  position = empty_positions(board).sample
+  board[position] = 'O'
 end
-
 
 def check_winner(board)
   winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
@@ -52,42 +44,30 @@ def check_winner(board)
   nil
 end
 
-    
 begin
 
-board = initialize_board #not sure what this does, or why we're setting it equal to board. 
-# maybe so that we could access it in the 'local scope?' i think it allows us to print out the hash b
+  board = initialize_board #not sure what this does, or why we're setting it equal to board. 
+  # maybe so that we could access it in the 'local scope?' i think it allows us to print out the hash b
 
-begin
-  draw_board(board)
-  player_pick_square(board)
-  draw_board(board)
-  winner = check_winner(board)
-  computer_picks_square(board)
-  winner = check_winner(board)
-end until winner || empty_positions(board).empty?
+  begin
+    draw_board(board)
+    player_pick_square(board)
+    draw_board(board)
+    winner = check_winner(board)
+    computer_picks_square(board)
+    winner = check_winner(board)
+  end until winner || empty_positions(board).empty?
 
-if winner == 'Player'
-  puts "You won!"
-elsif winner == 'Computer'
-  puts "Computers will soon run the land!"
-else 
-  puts "It's a tie!"
+  if winner == 'Player'
+    puts "You won!"
+  elsif winner == 'Computer'
+    puts "Computers will soon run the land!"
+  else 
+    puts "It's a tie!"
 
 end
-
-
 
 puts "Would you like to play again? (y/n)"
 play_again = gets.chomp.downcase
 end until play_again != 'y'
 puts "Adios!"
-
-
-
-
-
-
-
-
-
